58f4785d3539aa2529f6f2dc2b372581cd82d826
이주형 <joo@ijuhyeong-ui-MacBookAir.local>
2023-01-16 10:48:08 +0900

    [ADD]dice animation

--- client/main.js
+++ client/main.js
@@ -1,1 +1,14 @@
-console.log(window)
+import {diceAnimation, getNode} from './lib/index.js'
+
+const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)')
+
+// 설정한 초마다 호출
+// setInterval(()=>{
+//   console.log("안녕")
+// },1000)
+
+const handlerRollingDice = () =>{
+  diceAnimation()
+}
+
+rollingDiceButton.addEventListener('click', handlerRollingDice)
