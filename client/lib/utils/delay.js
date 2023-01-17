7ccd1a58fcb746cdd865b6cd21fa33330e0d5dc6
이주형 <joo@ijuhyeong-ui-MacBookAir.local>
2023-01-17 16:10:57 +0900

    비동기 통신

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -6,7 +6,7 @@ const delay = (callback, timeout = 1000) => {
   setTimeout(callback, timeout);
 };
 
-// 두 개가 같이 실행됨
+// 세 개가 같이 실행됨
 // 이런 경우가 callback이 필요한 경우임
 // first.style.top = "-100px";
 // first.style.transform = "rotate(360deg)";
