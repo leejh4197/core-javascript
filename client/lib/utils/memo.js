f33a347c5201920ad63b54e16535c15b2c69bada
이주형 <joo@ijuhyeong-ui-MacBookAir.local>
2023-01-17 17:03:26 +0900

    insertLast함수 수정

--- client/lib/utils/memo.js
+++ client/lib/utils/memo.js
@@ -1,17 +1,25 @@
-
-
 export const memo = (() => {
-    const cache = {}
-    return (key,callback) =>{
-        if(!callback) return cache[key];
+  const cache = {};
 
-        if(cache[key]){
-            console.warn(`${key} 값은 이미 캐시 된 값이 존재합니다.`);
-            return;
-        }
+  return (key, callback) => {
+    if (!callback) return cache[key];
 
-        cache[key] = callback();
+    if (cache[key]) {
+      console.warn(`${key} 값은 이미 캐시된 값이 존재합니다.`);
+      return;
     }
-})()
 
-// memo('cube',()=>document.querySelector("#cube"))
+    cache[key] = callback();
+
+    // console.log(cache);
+  };
+})();
+
+// memo('name',()=>'tiger')
+// memo('name')
+
+// console.log(memo('name',()=>'tttt'));
+
+// memo()('cube',()=> document.querySelector('#cube'));
+
+// console.log( memo()('cube') );
--- client/main.js
+++ client/main.js
@@ -1,4 +1,16 @@
-import {attr, clearContents, diceAnimation, disableElement, enableElenet, getNode, getNodes, insertLast, invisibleElement, memo, visibleElement} from './lib/index.js'
+import {
+  attr,
+  clearContents,
+  diceAnimation,
+  disableElement,
+  enableElenet,
+  getNode,
+  getNodes,
+  insertLast,
+  invisibleElement,
+  memo,
+  visibleElement,
+} from "./lib/index.js";
 
 // 설정한 초마다 호출
 // setInterval(()=>{
@@ -6,63 +18,59 @@ import {attr, clearContents, diceAnimation, disableElement, enableElenet, getNod
 // },1000)
 
 // 배열의 구조분해 할당
-const [rollingDiceButton, recordButton, resetButton] = getNodes('.buttonGroup > button');
-const recordListWrapper = getNode('.recordListWrapper')
-
-memo('@tbody', ()=>{getNode('recordListWrapper tbody')})
+const [rollingDiceButton, recordButton, resetButton] = getNodes(
+  ".buttonGroup > button"
+);
+const recordListWrapper = getNode(".recordListWrapper");
+memo("@tbody", () => getNode(".recordListWrapper tbody"));
 
 let count = 0;
 let total = 0;
 
 const renderRecordListItem = () => {
-  let diceValue = +attr(memo('cube'),'data-dice')
-  let template = /* html */`
+  let diceValue = Number(attr(memo("cube"), "data-dice"));
+  let template = /* html */ `
     <tr>
       <td>${++count}</td>
       <td>${diceValue}</td>
-      <td>${total += diceValue}</td>
+      <td>${(total += diceValue)}</td>
     </tr>
-  `
-  insertLast(memo('@tbody'),template)
-  recordListWrapper.scrollTop = recordListWrapper.scrollHeight
-  return template
-}
-
-
+  `;
+  insertLast(memo("@tbody"), template);
+  recordListWrapper.scrollTop = recordListWrapper.scrollHeight;
+};
 // event영역
 
-const handlerRollingDice = (() =>{
+const handlerRollingDice = (() => {
   let stopAnimation;
   let isRolling = false;
-  return () =>{
-    if(!isRolling){
-      stopAnimation = setInterval(diceAnimation,100)
-      disableElement(recordButton)
-      disableElement(resetButton)
-    }else{
-      clearInterval(stopAnimation)
-      enableElenet(recordButton)
-      enableElenet(resetButton)
+  return () => {
+    if (!isRolling) {
+      stopAnimation = setInterval(diceAnimation, 100);
+      disableElement(recordButton);
+      disableElement(resetButton);
+    } else {
+      clearInterval(stopAnimation);
+      enableElenet(recordButton);
+      enableElenet(resetButton);
     }
-    isRolling = !isRolling
-  }
+    isRolling = !isRolling;
+  };
+})();
 
-})()
-
-const recordHandler = () =>{
-  visibleElement(recordListWrapper)
+const recordHandler = () => {
+  visibleElement(recordListWrapper);
 
   renderRecordListItem();
-}
+};
 
-const resetHandler = () =>{
+const resetHandler = () => {
   count = 0;
   total = 0;
-  invisibleElement(recordListWrapper)
-  clearContents(memo('tbody'))
-  
-}
+  invisibleElement(recordListWrapper);
+  clearContents(memo("@tbody"));
+};
 
-rollingDiceButton.addEventListener('click', handlerRollingDice)
-recordButton.addEventListener('click',recordHandler)
-resetButton.addEventListener('click',resetHandler)
+rollingDiceButton.addEventListener("click", handlerRollingDice);
+recordButton.addEventListener("click", recordHandler);
+resetButton.addEventListener("click", resetHandler);
