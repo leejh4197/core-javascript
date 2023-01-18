fc6bb53431bf7e66ba3e0fde8ee8b8fca6e44b84
이주형 <joo@ijuhyeong-ui-MacBookAir.local>
2023-01-18 11:45:02 +0900

    promise 학습

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,6 +1,7 @@
 import { getNode } from "../dom/getNode.js";
 
 const first = getNode(".first");
+const second = getNode(".second");
 
 const delay = (callback, timeout = 1000) => {
   setTimeout(callback, timeout);
@@ -12,12 +13,62 @@ const delay = (callback, timeout = 1000) => {
 // first.style.transform = "rotate(360deg)";
 // first.style.top = "0px";
 
-delay(() => {
-  first.style.top = "-100px";
-  delay(() => {
-    first.style.transform = "rotate(360deg)";
-    delay(() => {
-      first.style.top = "0px";
-    });
+// delay(() => {
+//   first.style.top = "-100px";
+//   delay(() => {
+//     first.style.transform = "rotate(360deg)";
+//     delay(() => {
+//       first.style.top = "0px";
+//     });
+//   });
+// });
+
+// delayP().then(()=>{
+//   first.style.top = '-100px';
+//   return delayP()
+// }).then(()=>{
+//   first.style.transform = 'rotate(360deg)'
+//   second.style.
+// })
+
+const defaultOptions = {
+  shouldReject: false,
+  timeout: 1000,
+  data: "성공",
+  errorMessage: "알 수 없는 오류가 발생했습니다.",
+};
+
+const delayP = (options = {}) => {
+  // 얕은 복사
+  // defualt값이 변질되는 것을 방지하기 위해 직접 defaultOptions를 사용하지 않고 변수에 얕은 복사로 지정한다
+  let config = { ...defaultOptions };
+  // 둘다 똑같은 얕은 복사
+  // let config2 = Object.assign({},defaultOptions)
+
+  // 객체 합성 mixin
+  // 기존의 값과 던진 값을 합성 시켜주기 위해 객체를 합성한다. 뒤에 options가 덮어진다.
+  config = { ...config, ...options };
+
+  const { shouldReject, data, errorMessage, timeout } = config;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      !shouldReject ? resolve(data) : reject(errorMessage);
+    }, timeout);
   });
+};
+
+delayP({
+  shouldReject: true,
+  errorMessage: "잘가",
+  data: "안녕",
+  timeout: 1500,
+}).then((res) => {
+  console.log(res);
 });
+
+// delayP().then((res)=>{
+//   console.log(res)
+// }).catch((err)=>{
+//   console.log(err)
+// })
--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -1,5 +1,3 @@
-
-
 /* readyState
 0 : uninitalized // 초기화
 1 : loading // 로딩
@@ -8,101 +6,151 @@
 4 : complete // 완료
  */
 
-
+import { typeError } from "../error/typeError";
 
 export const xhrData = ({
-    url='',
-    method='GET',
-    onSuccess= null,
-    onFail=null,
-    body=null,
-    headers={
-        'Content-Type' : 'application/json',
-        'Access-Control-Allow-Origin':'*'
-    },
-}={}) =>{
+  url = "",
+  method = "GET",
+  onSuccess = null,
+  onFail = null,
+  body = null,
+  headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+} = {}) => {
+  const xhr = new XMLHttpRequest();
 
-    const xhr = new XMLHttpRequest();
-    
-    // 비동기 통신 오픈
-    xhr.open(method,url)
+  // 비동기 통신 오픈
+  xhr.open(method, url);
 
-    // Object.entries(headers).forEach(([key,value])=>{
-    //     console.log(xhr.setRequestHeader(key,value))
-    // })
-    
-    xhr.addEventListener('readystatechange',()=>{
-        // 객체 구조 분해 할당
-        const {status,readyState,response} = xhr;
-        if(status >= 200 && status < 400){
-            if(readyState === 4){
-                console.log('통신 성공')
-                onSuccess(JSON.parse(response))
-            }
-        }else{
-            onFail('통신실패')
-        }
-    })
-    
-    // 서버에 요청
-    xhr.send(JSON.stringify(body))
-}
+  // Object.entries(headers).forEach(([key,value])=>{
+  //     console.log(xhr.setRequestHeader(key,value))
+  // })
+
+  xhr.addEventListener("readystatechange", () => {
+    // 객체 구조 분해 할당
+    const { status, readyState, response } = xhr;
+    if (status >= 200 && status < 400) {
+      if (readyState === 4) {
+        console.log("통신 성공");
+        onSuccess(JSON.parse(response));
+      }
+    } else {
+      onFail("통신실패");
+    }
+  });
+
+  // 서버에 요청
+  xhr.send(JSON.stringify(body));
+};
 
 xhrData({
-    url:'https://jsonplaceholder.typicode.com/user/1',
-    onSuccess: (result)=>{
-        console.log(result)
-    },
-    onFail:(err)=>{
-        console.error(err)
-    }
-})
+  url: "https://jsonplaceholder.typicode.com/users/1",
+  onSuccess: (result) => {
+    console.log(result);
+  },
+  onFail: (err) => {
+    console.error(err);
+  },
+});
 
 // get 메서드
-xhrData.get = (url,onSuccess,onFail) =>{
-    xhrData({
-        url,
-        onSuccess,
-        onFail
-    })
-}
+xhrData.get = (url, onSuccess, onFail) => {
+  xhrData({
+    url,
+    onSuccess,
+    onFail,
+  });
+};
 
 // post 메서드
-xhrData.post = (url,body,onSuccess, onFail) =>{
-    xhrData({
-        method:'POST',
-        body,
-        url,
-        onSuccess,
-        onFail
-    })
-}
+xhrData.post = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method: "POST",
+    body,
+    url,
+    onSuccess,
+    onFail,
+  });
+};
 // put 메서드
-xhrData.put = (url,body,onSuccess, onFail) =>{
-    xhrData({
-        method:'PUT',
-        body,
-        url,
-        onSuccess,
-        onFail
-    })
-}
+xhrData.put = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method: "PUT",
+    body,
+    url,
+    onSuccess,
+    onFail,
+  });
+};
 // delete 메서드
-xhrData.delete = (url,onSuccess,onFail) =>{
-    xhrData({
-        method:'DELETE',
-        url,
-        onSuccess,
-        onFail
-    })
-}
+xhrData.delete = (url, onSuccess, onFail) => {
+  xhrData({
+    method: "DELETE",
+    url,
+    onSuccess,
+    onFail,
+  });
+};
 
 xhrData.post(
-    'https://jsonplaceholder.typicode.com/users',
-    (result)=>{
-        console.log(result)
-    },
-    (err)=>{
-        console.log(err)
-    }
-)
+  "https://jsonplaceholder.typicode.com/users",
+  (result) => {
+    console.log(result);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+// promise API
+
+const defaultOptions = {
+  url: "",
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+  body: null,
+};
+
+const xhrPromise = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+
+  const { method, url, body, headers } = Object.assign(
+    {},
+    defaultOptions,
+    options
+  );
+
+  if (!url) typeError("서버와 통신할 url 인자는 반드시 필요합니다.");
+
+  xhr.open(method, url);
+
+  xhr.send(body ? JSON.stringify(body) : null);
+  return new Promise((resolve, reject) => {
+    xhr.addEventListener("readystatechange", () => {
+      const { status, readyState, response } = xhr;
+
+      if (status >= 200 && status < 400) {
+        if (readyState === 4) {
+          resolve(JSON.parse(response));
+        } else {
+          reject("에러");
+        }
+      }
+    });
+  });
+};
+
+xhrPromise({
+  url: "https://jsonplaceholder.typicode.com/users/1",
+})
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
